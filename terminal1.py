d005e809c7a26394ab24a0b706e01cc43af40667
Farid Djadda <djaddafarid.contact@gmail.com>
2023-07-26 02:24:51 +0200

    code clean

--- terminal1.py
+++ terminal1.py
@@ -8,9 +8,11 @@ import subprocess
 
 while True:
     commande = input("Commande : ")
-    result = subprocess.run(commande, shell=True, capture_output=True, universal_newlines=True)  # ls sur Linux/macOS
-    print(result.stdout)
     if commande == "exit":
         break
+    
+    result = subprocess.run(commande, shell=True, capture_output=True, universal_newlines=True)  # ls sur Linux/macOS
+    
+    print(result.stdout)
     print(result.stderr)
     
