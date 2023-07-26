b9a7776cf1061b99794bf01cca9dbedf651feb59
Farid Djadda <djaddafarid.contact@gmail.com>
2023-07-26 02:22:12 +0200

    Request commands from the user

--- terminal1.py
+++ terminal1.py
@@ -4,7 +4,13 @@ import subprocess
 
 # Popen : ancienne interface
 # run : executer la commande et attendre le résultat
-result = subprocess.run("dir", shell=True, capture_output=True, universal_newlines=True)  # ls sur Linux/macOS
 
-print(result.stdout)
-print(result.stderr)
+
+while True:
+    commande = input("Commande : ")
+    result = subprocess.run(commande, shell=True, capture_output=True, universal_newlines=True)  # ls sur Linux/macOS
+    print(result.stdout)
+    if commande == "exit":
+        break
+    print(result.stderr)
+    
